24183dd9d0cf8192cf755c7fb4918f9fbaa059d0
Majid Behzadnasab <mbehzad71@gmail.com>
2021-09-01 00:36:50 +0430

    Add files via upload

--- src/CompressionServiceProvider.php
+++ src/CompressionServiceProvider.php
@@ -4,7 +4,6 @@
 namespace TenetUp\Compression;
 
 
-use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class CompressionServiceProvider extends ServiceProvider
@@ -14,13 +13,8 @@ class CompressionServiceProvider extends ServiceProvider
         $this->app->bind('Compression' , function (){
             return new Compression;
         });
-        $loader = AliasLoader::getInstance();
-        $loader->alias('Compression', CompressionFacade::class);
-//        $this->mergeConfigFrom(__DIR__ . '\config.php' , 'Compression');
     }
     public function boot(){
-//        $this->publishes([
-//            __DIR__.'/config.php' => config_path('zarinpal.php'),
-//        ]);
+
     }
 }
